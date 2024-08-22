fdc9cf95b09fb60b489a005bebdb808e3635e325
An7h0ny1 <91570972+An7h0ny1@users.noreply.github.com>
2024-08-22 14:39:59 -0500

    first commit

---
+++ Backend/src/main/java/com/gestor_clinica_veterinaria/VeterinaryHospitalManager/VeterinaryHospitalManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gestor_clinica_veterinaria.VeterinaryHospitalManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinaryHospitalManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinaryHospitalManagerApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/gestor_clinica_veterinaria/VeterinaryHospitalManager/VeterinaryHospitalManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestor_clinica_veterinaria.VeterinaryHospitalManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryHospitalManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
