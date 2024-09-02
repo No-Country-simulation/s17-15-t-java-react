19e6a99d5c94ccc32be0fae98e4c52c14925ec4b
An7h0ny1 <91570972+An7h0ny1@users.noreply.github.com>
2024-09-02 15:23:43 -0500

    pruebas conexion

--- Backend/src/main/resources/import.sql
+++ Backend/src/main/resources/import.sql
@@ -1,26 +1,26 @@
 -- Insertar permisos
-INSERT INTO permissions (id, name) VALUES (1, 'CREATE');
-INSERT INTO permissions (id, name) VALUES (2, 'READ');
-INSERT INTO permissions (id, name) VALUES (3, 'WRITE');
-INSERT INTO permissions (id, name) VALUES (4, 'DELETE');
+INSERT INTO permissions (id, name) VALUES (1, 'CREATE') ON CONFLICT DO NOTHING;
+INSERT INTO permissions (id, name) VALUES (2, 'READ') ON CONFLICT DO NOTHING;
+INSERT INTO permissions (id, name) VALUES (3, 'WRITE') ON CONFLICT DO NOTHING;
+INSERT INTO permissions (id, name) VALUES (4, 'DELETE') ON CONFLICT DO NOTHING;
 
 -- Insertar roles
-INSERT INTO roles (id, role_name) VALUES (1, 'ADMIN');
-INSERT INTO roles (id, role_name) VALUES (2, 'USER');
+INSERT INTO roles (id, role_name) VALUES (1, 'ADMIN') ON CONFLICT DO NOTHING;
+INSERT INTO roles (id, role_name) VALUES (2, 'USER') ON CONFLICT DO NOTHING;
 
 -- Relacionar roles con permisos
-INSERT INTO role_permission (role_id, permission_id) VALUES (1, 1); -- ADMIN con CREATE
-INSERT INTO role_permission (role_id, permission_id) VALUES (1, 2); -- ADMIN con READ
-INSERT INTO role_permission (role_id, permission_id) VALUES (1, 3); -- ADMIN con WRITE
-INSERT INTO role_permission (role_id, permission_id) VALUES (1, 4); -- ADMIN con DELETE
+INSERT INTO role_permission (role_id, permission_id) VALUES (1, 1) ON CONFLICT DO NOTHING; -- ADMIN con CREATE
+INSERT INTO role_permission (role_id, permission_id) VALUES (1, 2) ON CONFLICT DO NOTHING; -- ADMIN con READ
+INSERT INTO role_permission (role_id, permission_id) VALUES (1, 3) ON CONFLICT DO NOTHING; -- ADMIN con WRITE
+INSERT INTO role_permission (role_id, permission_id) VALUES (1, 4) ON CONFLICT DO NOTHING; -- ADMIN con DELETE
 
-INSERT INTO role_permission (role_id, permission_id) VALUES (2, 2); -- USER con READ
+INSERT INTO role_permission (role_id, permission_id) VALUES (2, 2) ON CONFLICT DO NOTHING; -- USER con READ
 
 
-INSERT INTO users (id, username, password, email, is_enabled, account_no_expired, account_no_locked, credentials_no_expired) VALUES (100000, 'admin', '$2a$10$Dxnp0wJeF8L9Ftd8./ExcuaJ62jKKVezB3gvBLcgo1qNOjAnR6QJi', 'admin@example.com', true, true, true, true);
+INSERT INTO users (id, username, password, email, is_enabled, account_no_expired, account_no_locked, credentials_no_expired) VALUES (100000, 'admin', '$2a$10$Dxnp0wJeF8L9Ftd8./ExcuaJ62jKKVezB3gvBLcgo1qNOjAnR6QJi', 'admin@example.com', true, true, true, true) ON CONFLICT DO NOTHING;
 --INSERT INTO user_entity (id, username, password, email, is_enabled, account_no_expired, account_no_locked, credentials_no_expired, user_type, apellido, especialidad, tarjeta_profesional) VALUES (100000, 'admin', '$2a$10$Dxnp0wJeF8L9Ftd8./ExcuaJ62jKKVezB3gvBLcgo1qNOjAnR6QJi', 'admin@example.com', true, true, true, true, 'VETERINARIO', 'admin', 'veterinario', '12345');
 
 -- Relacionar el usuario administrador con el rol ADMIN
-INSERT INTO users_roles (user_id, role_id) VALUES (100000, 1); -- el rol ADMIN al usuario 'admin'
+INSERT INTO users_roles (user_id, role_id) VALUES (100000, 1) ON CONFLICT DO NOTHING; -- el rol ADMIN al usuario 'admin'
 
 
