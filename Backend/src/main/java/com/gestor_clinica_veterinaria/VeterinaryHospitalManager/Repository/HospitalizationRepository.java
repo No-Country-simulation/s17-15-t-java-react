06a9976745756e389c2bdc23ad94154933252638
Alexis Alejandro Calderon <alexiscalderon202@gmail.com>
2024-09-18 03:19:18 -0300

    Get hospitalization by id pet

--- Backend/src/main/java/com/gestor_clinica_veterinaria/VeterinaryHospitalManager/Repository/HospitalizationRepository.java
+++ Backend/src/main/java/com/gestor_clinica_veterinaria/VeterinaryHospitalManager/Repository/HospitalizationRepository.java
@@ -2,6 +2,8 @@ package com.gestor_clinica_veterinaria.VeterinaryHospitalManager.Repository;
 
 import com.gestor_clinica_veterinaria.VeterinaryHospitalManager.Entity.Hospitalization;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +15,10 @@ public interface HospitalizationRepository extends JpaRepository<Hospitalization
     List<Hospitalization> findByTreatments_Id(Long treatmentId);
 
     List<Hospitalization> findByComplementaryStudies_Id(Long complementaryStudyId);
+
+    @Query("SELECT h FROM Hospitalization h JOIN h.complementaryStudies cs JOIN cs.consultation c WHERE c.pet.id = :petId")
+    List<Hospitalization> findHospitalizationsByPetId(@Param("petId") Long petId);
+
+    @Query("SELECT h FROM Hospitalization h JOIN h.treatments t JOIN t.diagnosis d JOIN d.consultation c WHERE c.pet.id = :petId")
+    List<Hospitalization> findHospitalizationsByPetIdTreatment(@Param("petId") Long petId);
 }
